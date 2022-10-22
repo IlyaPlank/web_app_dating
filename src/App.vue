6411eeb73597f3ec005e1996c47c47f220bb631c
Ilya Plank <ilplank@yandex.ru>
2022-10-22 17:44:37 +0500

    start

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+const tg = window.Telegram.WebApp
+
+// const isLoading = ref(true)
+
+tg.ready()
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="drawerOpen = !drawerOpen"
+        />
+
+        <q-toolbar-title> Quasar App </q-toolbar-title>
+
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="drawerOpen" show-if-above bordered>
+      <q-list> sdfsdf </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const drawerOpen = ref(false)
+</script>
+
+<style lang="scss" scoped></style>
